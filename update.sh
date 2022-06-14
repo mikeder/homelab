994c4d9310b1275905863342063a29422a8b8762
Mike Eder <sqweebking@gmail.com>
2022-06-13 22:33:38 -0400

    forceupdate

---
+++ update.sh
@@ -0,0 +1,10 @@
+!/bin/bash
+
+for d in */ ; do
+    echo "$d"
+    docker-compose -f $d/docker-compose.yml pull
+    docker-compose -f $d/docker-compose.yml down
+    docker-compose -f $d/docker-compose.yml up -d
+done
+
+docker system prune -af
