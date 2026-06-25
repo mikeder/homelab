59c2d0bed1ee857d51a1b7041e657a3a2054425a
Mike Eder <sqweebking@gmail.com>
2026-06-24 22:09:28 -0400

    clenaup

---
+++ wireguard/ha.sh
@@ -0,0 +1,2 @@
+#docker run ghcr.io/wg-easy/wg-easy:14 node -e 'const bcrypt = require("bcryptjs"); const hash = bcrypt.hashSync("XbNK%*t83&@R9B%^PB$8",10); console.log(hash.replace(/\$/g, "$$$$"));'
+docker run ghcr.io/wg-easy/wg-easy:14 wgpw 4qod$Jc@74cFDr%$$6UV
